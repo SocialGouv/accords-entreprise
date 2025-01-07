18720eaea556530414261a2aecb883b988dab836
Victor DEGLIAME <victor.degliame@gmail.com>
2025-01-07 01:30:59 +0100

    feat: Ensure all embedding clients will return normalized embeddings

--- tca/embedding/embedding_clients.py
+++ tca/embedding/embedding_clients.py
@@ -1,3 +1,4 @@
+import numpy as np
 import ollama
 
 from tca.custom_types import Embeddings
@@ -5,6 +6,16 @@ from tca.custom_types import Embeddings
 
 class BaseEmbeddingClient:
     def embed(self, texts: list[str]) -> list[Embeddings]:
+        embeddings = self._embed(texts)
+        normalized_embeddings = [
+            np.array(embedding) / np.linalg.norm(embedding) for embedding in embeddings
+        ]
+        normalized_embeddings = [
+            embedding.tolist() for embedding in normalized_embeddings
+        ]
+        return normalized_embeddings  # type: ignore
+
+    def _embed(self, texts: list[str]) -> list[Embeddings]:
         raise NotImplementedError("Subclasses should implement this method")
 
 
@@ -12,7 +23,7 @@ class OllamaEmbeddingClient(BaseEmbeddingClient):
     def __init__(self, model="bge-m3:567m-fp16"):
         self.model = model
 
-    def embed(self, texts: list[str]) -> list[Embeddings]:
+    def _embed(self, texts: list[str]) -> list[Embeddings]:
         embeddings_response = ollama.embed(
             model=self.model,
             input=texts,
