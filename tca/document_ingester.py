3e70da057f78b2cf89f2c7e5fe542b60595a9663
Victor DEGLIAME <victor.degliame@gmail.com>
2025-01-08 17:32:24 +0100

    refactor: Document ingestion

--- tca/document_ingester.py
+++ tca/document_ingester.py
@@ -1,57 +1,22 @@
-import glob
 import hashlib
 import logging
 import logging.config
 import os
 from pathlib import Path
 
-from tca.constants import DATA_FOLDER
 from tca.document_chunk_manager import DocumentChunkManager
-from tca.text.document_utils import DocumentUtils
-
-INPUT_FILE_PREFIXES = [
-    "T05123005651",
-    "A05114000445",
-    "T09224067466",
-    "T00624061516",
-    "T07624061950",
-    "T04524061140",
-    "T08722002525",
-    "T01023002490",
-    "T00624061500",
-    "T09524061379",
-    "T03624060237",
-    "T07424061122",
-    "T09224067135",
-    "T09423011056",
-    "T03424061573",
-    "T01424061178",
-    "T00624061455",
-    "T06024060960",
-    "T08424060468",
-    "T20A23060010",
-    "T09023001809",
-    "T09523006784",
-    "T01423060053",
-    "T09219008191",
-]
+from tca.text.document_utils import DocumentLoader
 
 
 class DocumentIngester:
     def __init__(self, document_chunk_manager: DocumentChunkManager):
         self.document_chunk_manager = document_chunk_manager
 
-    def ingest_documents(self) -> None:
-        documents_folder = f"{DATA_FOLDER}/accords_entreprise_niveau2"
-        document_paths: list[Path] = []
-        for prefix in INPUT_FILE_PREFIXES:
-            document_paths.extend(
-                Path(file) for file in glob.glob(f"{documents_folder}/{prefix}*")
-            )
-
+    def ingest_documents(self, document_paths: list[Path]) -> None:
         for document_path in document_paths:
             logging.info('Processing document "%s"', document_path)
-            document_text = DocumentUtils.extract_text_from_document(document_path)
+            document_loaders = DocumentLoader()
+            document_text = document_loaders.load_text_from_document(document_path)
             document_id = hashlib.sha256(document_text.encode()).hexdigest()
             logging.info('Extracting chunks from document "%s"', document_path)
             doc_chunks = self.document_chunk_manager.chunk_document(document_text)
--- tca/text/document_utils.py
+++ tca/text/document_utils.py
@@ -1,22 +1,48 @@
 from pathlib import Path
 
-import docx
-import textract
+from doc2docx import convert
+from langchain_community.document_loaders.word_document import Docx2txtLoader
 from PyPDF2 import PdfReader
 
+from tca.constants import DATA_FOLDER
 
-class DocumentUtils:
-    @staticmethod
-    def extract_text_from_document(document_file: Path):
-        if document_file.suffix == ".pdf":
-            with open(document_file, "rb") as file:
-                reader = PdfReader(file)
-                return "\n".join(
-                    [reader.pages[i].extract_text() for i in range(len(reader.pages))]
-                )
-        elif document_file.suffix == ".doc":
-            return textract.process(str(document_file)).decode("utf-8")
-        elif document_file.suffix == ".docx":
-            doc = docx.Document(str(document_file))
-            return "\n".join([para.text for para in doc.paragraphs])
+
+class DocumentLoader:
+    def __init__(self):
+        converted_docx_folder = Path(f"{DATA_FOLDER}/converted_docx")
+        converted_docx_folder.mkdir(parents=True, exist_ok=True)
+
+    def load_text_from_pdf_file(self, document_file: Path):
+        with open(document_file, "rb") as file:
+            reader = PdfReader(file)
+            return "\n".join(
+                [reader.pages[i].extract_text() for i in range(len(reader.pages))]
+            )
+
+    def load_text_from_docx_file(self, document_file: Path):
+        docx_loader = Docx2txtLoader(document_file)
+        documents = docx_loader.load()
+
+        return "\n".join([doc.page_content for doc in documents])
+
+    def load_text_from_doc_file(self, document_file: Path):
+        new_document_name = document_file.stem
+        # TODO: Handle the saving and potential caching + overwritting of the new document in a more sustainable way
+        new_document_path = Path(
+            f"{DATA_FOLDER}/converted_docx/{new_document_name}.docx"
+        )
+        convert(str(document_file), new_document_path)
+
+        return self.load_text_from_docx_file(new_document_path)
+
+    def load_text_from_document(self, document_file: Path):
+        extractors = {
+            ".pdf": self.load_text_from_pdf_file,
+            ".docx": self.load_text_from_docx_file,
+            ".doc": self.load_text_from_doc_file,
+        }
+
+        extractor = extractors.get(document_file.suffix)
+        if extractor:
+            return extractor(document_file)
         return ""
